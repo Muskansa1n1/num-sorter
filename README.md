7164cf7879f1481007296745cd2f0ab5d5c319f1
Muskan Saini <muskansaini0005@gmail.com>
2024-06-22 18:07:13 +0530

    removed dependencies

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# sort-nums
+# num-sorter
 
 A simple npm library for sorting numbers in ascending order.
 
@@ -7,7 +7,7 @@ A simple npm library for sorting numbers in ascending order.
 Install the library using npm:
 
 ```
-$ npm install sort-nums
+$ npm install num-sorter
 
 ```
 
@@ -17,21 +17,21 @@ Import the library into your project and use it to sort numbers or arrays of num
 
 ```js
 
-const sortNums = require('sort-nums');
+const numSorter = require('num-sorter');
 
-console.log(sortNums(3, 1, 4, 1, 5, 9)); // [1, 1, 3, 4, 5, 9]
+console.log(numSorter(3, 1, 4, 1, 5, 9)); // [1, 1, 3, 4, 5, 9]
 
 const numbers = [3, 1, 4, 1, 5, 9];
-const sortedNumbers = sortNums(numbers);
+const sortedNumbers = numSorter(numbers);
 console.log(sortedNumbers); // [1, 1, 3, 4, 5, 9]
 
-console.log(sortNums(5)); // [5]
+console.log(numSorter(5)); // [5]
 
 ```
 
 # API
 
-### sortNums(numbers)
+### numSorter(numbers)
 Sorts numbers or an array of numbers in ascending order.
 
 #### Parameters
--- index.test.js
+++ index.test.js
@@ -1,23 +1,23 @@
-const sortNums = require('./index');
+const numSorter = require('./index');
 
 test('sorts an array of numbers in ascending order', () => {
-  expect(sortNums([3, 1, 4, 1, 5, 9])).toEqual([1, 1, 3, 4, 5, 9]);
+  expect(numSorter([3, 1, 4, 1, 5, 9])).toEqual([1, 1, 3, 4, 5, 9]);
 });
 
 test('sorts individual numbers in ascending order', () => {
-  expect(sortNums(3, 1, 4, 1, 5, 9)).toEqual([1, 1, 3, 4, 5, 9]);
+  expect(numSorter(3, 1, 4, 1, 5, 9)).toEqual([1, 1, 3, 4, 5, 9]);
 });
 
 test('returns a single number in an array', () => {
-  expect(sortNums(5)).toEqual([5]);
+  expect(numSorter(5)).toEqual([5]);
 });
 
 test('returns an empty array when no input is given', () => {
-  expect(sortNums()).toEqual([]);
+  expect(numSorter()).toEqual([]);
 });
 
 test('throws a TypeError when input is not valid', () => {
-  expect(() => sortNums(3, 'a', 4)).toThrow(TypeError);
-  expect(() => sortNums(null)).toThrow(TypeError);
-  expect(() => sortNums({})).toThrow(TypeError);
+  expect(() => numSorter(3, 'a', 4)).toThrow(TypeError);
+  expect(() => numSorter(null)).toThrow(TypeError);
+  expect(() => numSorter({})).toThrow(TypeError);
 });
